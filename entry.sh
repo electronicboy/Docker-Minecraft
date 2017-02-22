e130618da08b0a24093dc11ad4aa97f8ad598101
Shane Freeder <theboyetronic@gmail.com>
2017-02-22 12:31:05 +0000

    replace the invocations of tput with escape sequences

--- entry.sh
+++ entry.sh
@@ -1,9 +1,9 @@
 #!/bin/ash
 sleep 5
 
-C_RED=`tput setaf 1`
-C_GREEN=`tput setaf 2`
-C_RESET=`tput sgr0`
+C_RED='\e[0;31m'
+C_GREEN='\e[0;32m'
+C_RESET='\e[0m'
 
 # Determine the latest version, or set the version to download.
 if [ -z "${BUNGEE_VERSION}" ] || [ "${BUNGEE_VERSION}" == "latest" ]; then
@@ -16,12 +16,12 @@ fi
 if [ "${SERVER_JARFILE}" == "waterfall.jar" ]; then
     SERVER_JARFILE="waterfall.jar"
     if [ -f "/home/container/${SERVER_JARFILE}" ]; then
-        echo "${C_GREEN}Found ${SERVER_JARFILE} in container, not downloading a new jar.${C_RESET}"
+        echo -e "${C_GREEN}Found ${SERVER_JARFILE} in container, not downloading a new jar.${C_RESET}"
     else
         echo ":/home/container$ curl -sS https://ci.destroystokyo.com/job/Waterfall/${DL_VERSION}/artifact/Waterfall-Proxy/bootstrap/target/Waterfall.jar -o ${SERVER_JARFILE}"      
         curl -sS https://ci.destroystokyo.com/job/Waterfall/${DL_VERSION}/artifact/Waterfall-Proxy/bootstrap/target/Waterfall.jar -o ${SERVER_JARFILE}
         if [ $? -ne 0 ]; then
-            echo "${C_RED}PTDL_CONTAINER_ERR: There was an error while attempting to download a new jarfile for this server.${C_RESET}"
+            echo -e "${C_RED}PTDL_CONTAINER_ERR: There was an error while attempting to download a new jarfile for this server.${C_RESET}"
             exit 1
         fi
     fi
@@ -30,12 +30,12 @@ elif [ -z "${SERVER_JARFILE}" ] || [ "${SERVER_JARFILE}" == "bungeecord.jar" ]; 
     SERVER_JARFILE="bungeecord.jar"
 # Download the correct version, or skip if it already exists.
     if [ -f "/home/container/${SERVER_JARFILE}" ]; then
-        echo "${C_GREEN}Found ${SERVER_JARFILE} in container, not downloading a new jar.${C_RESET}"
+        echo -e "${C_GREEN}Found ${SERVER_JARFILE} in container, not downloading a new jar.${C_RESET}"
     else
         echo ":/home/container$ curl -sS http://ci.md-5.net/job/BungeeCord/${DL_VERSION}/artifact/bootstrap/target/BungeeCord.jar -o ${SERVER_JARFILE}"
         curl -sS http://ci.md-5.net/job/BungeeCord/${DL_VERSION}/artifact/bootstrap/target/BungeeCord.jar -o ${SERVER_JARFILE}
         if [ $? -ne 0 ]; then
-            echo "${C_RED}PTDL_CONTAINER_ERR: There was an error while attempting to download a new jarfile for this server.${C_RESET}"
+            echo -e "${C_RED}PTDL_CONTAINER_ERR: There was an error while attempting to download a new jarfile for this server.${C_RESET}"
             exit 1
         fi
     fi
@@ -52,6 +52,6 @@ echo ":/home/container$ java ${MODIFIED_STARTUP}"
 java ${MODIFIED_STARTUP}
 
 if [ $? -ne 0 ]; then
-    echo "${C_RED}PTDL_CONTAINER_ERR: There was an error while attempting to run the start command.${C_RESET}"
+    echo -e "${C_RED}PTDL_CONTAINER_ERR: There was an error while attempting to run the start command.${C_RESET}"
     exit 1
 fi
